14d97450d9c1bf6153b26357b9f64b28c7b9e512
harshattray <harsh.attray@gmail.com>
2020-12-28 13:39:14 +0100

    Multiple changes including fetching Card Data and Card Flip

--- index.js
+++ index.js
@@ -16,9 +16,69 @@ import {
 	cardUserName,
 	exp,
 	cardlogo,
-	notice,
-	noticeText,
 	userData,
 	cardInfo,
 	card,
 } from "./utils.js";
+
+/**
+ * fetches the Year/Month values
+ */
+
+year.addEventListener("input", () => {
+	exp.textContent = `${month.value}/${year.value}`;
+});
+
+cardNumber.addEventListener("input", () => {
+	if (cardNumber.textLength != 16) {
+		err.classList.add("err");
+	} else {
+		err.classList.remove("err");
+	}
+	const num = cardNumber.value;
+	let firstFour = num.slice(0, 4);
+	let secondFour = num.slice(4, 8);
+	let thirdFour = num.slice(8, 12);
+	let fourthfour = num.slice(12);
+	first.textContent = firstFour;
+	second.textContent = secondFour;
+	third.textContent = thirdFour;
+	fourth.textContent = fourthfour;
+});
+
+//handling Card holder an its update
+cardHolder.addEventListener("input", function () {
+	let holder = this.value;
+	cardUserName.textContent = this.value;
+});
+
+/**
+ * Handles card Flip
+ */
+
+const rotate = () => {
+	card.classList.toggle("rotateF");
+	backcard.classList.toggle("rotateB");
+};
+cvv.addEventListener("focus", rotate);
+cvv.addEventListener("blur", rotate);
+
+cvv.addEventListener("input", (e) => {
+	cvvSpan.textContent = cvv.value;
+});
+
+/**
+ * Form submit ~ fetch values and clear on success
+ */
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+	if (cardNumber.textLength != 16) {
+		e.preventDefault();
+	} else {
+		cvv.textContent = " ";
+		cardNumber.textContent = " ";
+		cardHolder.textContent = " ";
+		month.textContent = " ";
+		year.textContent = " ";
+	}
+});
